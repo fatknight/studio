979c8d804925f3e9a2a7e22a95f0a422f3a4e37e
St Peter's & St Paul's JSO Church <mathikerejacobitechurch@gmail.com>
2025-08-18 14:55:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,4 @@
 
-
-// ==========================================================================================
-// !! IMPORTANT !!
-// YOUR APP WILL NOT WORK UNTIL YOU COMPLETE THE STEPS BELOW.
-//
-// This file is trying to connect to Firebase, but it's using placeholder
-// values. You need to replace them with the actual configuration from your
-// Firebase project.
-//
-// HOW TO FIX THIS:
-// 1. Go to the Firebase Console: https://console.firebase.google.com/
-// 2. Open your project.
-// 3. Go to "Project settings" (click the gear icon ⚙️).
-// 4. In the "General" tab, scroll down to the "Your apps" section.
-// 5. Click on your web app (or create one if you haven't).
-// 6. Find the `firebaseConfig` object and copy its values.
-// 7. Paste the values into the `firebaseConfig` object below, replacing the "TODO: ..." placeholders.
-// ==========================================================================================
-
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
@@ -27,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAXyI-rMJ2FJiVZfdibxYZfM7PenFHLcpY",
   authDomain: "verity-directory.firebaseapp.com",
   projectId: "verity-directory",
-  storageBucket: "verity-directory.firebasestorage.app",
+  storageBucket: "verity-directory.appspot.com",
   messagingSenderId: "389378794700",
   appId: "1:389378794700:web:3d036b3cea58c6f9e48e69"
 };

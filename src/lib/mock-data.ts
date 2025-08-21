25a58ab00dadadc2a990c117db4d8338d312fa21
St Peter's & St Paul's JSO Church <mathikerejacobitechurch@gmail.com>
2025-08-21 08:25:37 +0000

    looks like the memebr of sub group has been removed.. please add all mem

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -83,6 +83,7 @@ export const members: Member[] = [
     zone: '',
     ward: '',
     role: 'Admin',
+    subGroups: ['Church Committee'],
   },
   {
     id: '1',
@@ -99,14 +100,14 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2010-06-12',
     family: [
-      { email: 'jane.doe@example.com', status: 'Active', relation: 'Spouse', name: 'Jane Doe', birthday: '1988-04-12', phone: '555-0113', memberPhotoUrl: 'https://placehold.co/128x128/D5F5E3/1E8449.png', subGroups: ['Choir'], maritalStatus: 'Married', weddingDay: '2010-06-12' },
-      { email: 'jimmy.doe@example.com', status: 'Active', relation: 'Son', name: 'Jimmy Doe', birthday: '2012-01-15', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/E8DAEF/884EA0.png', subGroups: ['Altar Servers'], maritalStatus: 'Married', weddingDay: '2015-08-15' },
-      { email: 'jenny.doe@example.com', status: 'Inactive', relation: 'Daughter-in-law', name: 'Jenny Doe', birthday: '2014-07-22', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/FDEBD0/D35400.png', subGroups: ['Sunday School'], maritalStatus: 'Married', weddingDay: '2015-08-15' },
+      { email: 'jane.doe@example.com', status: 'Active', relation: 'Spouse', name: 'Jane Doe', birthday: '1988-04-12', phone: '555-0113', memberPhotoUrl: 'https://placehold.co/128x128/D5F5E3/1E8449.png', subGroups: ['Womens League'], maritalStatus: 'Married', weddingDay: '2010-06-12' },
+      { email: 'jimmy.doe@example.com', status: 'Active', relation: 'Son', name: 'Jimmy Doe', birthday: '2012-01-15', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/E8DAEF/884EA0.png', subGroups: ['Altar'], maritalStatus: 'Single'},
+      { email: 'jenny.doe@example.com', status: 'Inactive', relation: 'Daughter-in-law', name: 'Jenny Doe', birthday: '2014-07-22', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/FDEBD0/D35400.png', subGroups: ['Sunday School'], maritalStatus: 'Single' },
     ],
     familyName: 'Doe Family',
     familyId: '18/PM/0001',
     familyPhotoUrl: 'https://placehold.co/600x400.png',
-    subGroups: ['Sunday School', 'Choir'],
+    subGroups: ['Church Committee'],
     memberPhotoUrl: 'https://placehold.co/128x128/EBF4FA/3E6680.png',
     zone: 'North Zone',
     ward: 'Ward 1',
@@ -127,15 +128,14 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2015-08-14',
     family: [
-      { email: 'john.smith@example.com', status: 'Active', relation: 'Spouse', name: 'John Smith', birthday: '1989-11-30', phone: '555-0114', memberPhotoUrl: 'https://placehold.co/128x128/D6EAF8/2E86C1.png', subGroups: ['Men\'s Fellowship'], maritalStatus: 'Married', weddingDay: '2015-08-14' },
+      { email: 'john.smith@example.com', status: 'Active', relation: 'Spouse', name: 'John Smith', birthday: '1989-11-30', phone: '555-0114', memberPhotoUrl: 'https://placehold.co/128x128/D6EAF8/2E86C1.png', subGroups: ['Elders Forum'], maritalStatus: 'Married', weddingDay: '2015-08-14' },
       { email: 'jake.smith@example.com', status: 'Active', relation: 'Son', name: 'Jake Smith', birthday: '2018-06-01', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/D1F2EB/138D75.png', subGroups: ['Sunday School'], maritalStatus: 'Single' },
-      { email: 'jake.smith@example.com', status: 'Active', relation: 'Daughter-in-law', name: 'Jake Smith', birthday: '2018-06-01', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/D1F2EB/138D75.png', subGroups: ['Sunday School'], maritalStatus: 'Single' },
-      { email: 'jill.smith@example.com', status: 'Active', relation: 'Mother', name: 'Jill Smith', birthday: '1965-02-10', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/F5EEF8/6C3483.png', maritalStatus: 'Widowed' }
+      { email: 'jill.smith@example.com', status: 'Active', relation: 'Mother', name: 'Jill Smith', birthday: '1965-02-10', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/F5EEF8/6C3483.png', subGroups: ['Elders Forum'], maritalStatus: 'Widowed' }
     ],
     familyName: 'Smith Family',
     familyId: '19/AM/0002',
     familyPhotoUrl: 'https://placehold.co/600x400.png',
-    subGroups: ['Youth League'],
+    subGroups: ['Youth Association'],
     memberPhotoUrl: 'https://placehold.co/128x128/FCF3CF/F1C40F.png',
     zone: 'North Zone',
     ward: 'Ward 2',
@@ -157,7 +157,7 @@ export const members: Member[] = [
     family: [],
     familyName: 'Jones Family',
     familyId: '17/PM/0003',
-    subGroups: ['Men\'s Fellowship'],
+    subGroups: ['Elders Forum'],
     memberPhotoUrl: 'https://placehold.co/128x128/F5B7B1/922B21.png',
     zone: 'South Zone',
     ward: 'Ward 1',
@@ -177,11 +177,11 @@ export const members: Member[] = [
     birthday: '1995-03-25',
     maritalStatus: 'Single',
     family: [
-       { email: 'chris.johnson@example.com', status: 'Active', relation: 'Son', name: 'Chris Johnson', birthday: '2020-05-20', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/E6E6FA/483D8B.png', maritalStatus: 'Single' }
+       { email: 'chris.johnson@example.com', status: 'Active', relation: 'Son', name: 'Chris Johnson', birthday: '2020-05-20', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/E6E6FA/483D8B.png', subGroups: ['Sunday School'], maritalStatus: 'Single' }
     ],
     familyName: 'Johnson Family',
     familyId: '20/PM/0004',
-    subGroups: ['Sunday School Teacher'],
+    subGroups: ['Sunday School'],
     memberPhotoUrl: 'https://placehold.co/128x128/D7BDE2/5B2C6F.png',
     zone: 'South Zone',
     ward: 'Ward 2',
@@ -202,11 +202,11 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2005-02-14',
     family: [
-      { email: 'susan.williams@example.com', status: 'Active', relation: 'Spouse', name: 'Susan Williams', birthday: '1980-02-20', phone: '555-0115', memberPhotoUrl: 'https://placehold.co/128x128/FFDAB9/A0522D.png', maritalStatus: 'Married', weddingDay: '2005-02-14' }
+      { email: 'susan.williams@example.com', status: 'Active', relation: 'Spouse', name: 'Susan Williams', birthday: '1980-02-20', phone: '555-0115', memberPhotoUrl: 'https://placehold.co/128x128/FFDAB9/A0522D.png', subGroups: ['Womens League'], maritalStatus: 'Married', weddingDay: '2005-02-14' }
     ],
     familyName: 'Williams Family',
     familyId: '21/AM/0005',
-    subGroups: ['Pastoral Council'],
+    subGroups: ['Church Committee'],
     memberPhotoUrl: 'https://placehold.co/128x128/FAD7A0/AF601A.png',
     zone: 'East Zone',
     ward: 'Ward 1',
@@ -228,6 +228,7 @@ export const members: Member[] = [
     family: [],
     familyName: 'Brown Family',
     familyId: '16/PM/0006',
+    subGroups: ['Elders Forum'],
     memberPhotoUrl: 'https://placehold.co/128x128/ABEBC6/196F3D.png',
     zone: 'East Zone',
     ward: 'Ward 2',
@@ -248,13 +249,13 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2008-08-20',
     family: [
-      { email: 'patricia.davis@example.com', status: 'Active', relation: 'Spouse', name: 'Patricia Davis', birthday: '1982-08-20', phone: '555-0116', memberPhotoUrl: 'https://placehold.co/128x128/B0E0E6/4682B4.png', subGroups: ['Charity Wing'], maritalStatus: 'Married', weddingDay: '2008-08-20' },
-      { email: 'robert.davis@example.com', status: 'Active', relation: 'Son', name: 'Robert Davis', birthday: '2010-10-10', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/B0E0E6/4682B4.png', maritalStatus: 'Single' },
-      { email: 'jennifer.davis@example.com', status: 'Active', relation: 'Daughter', name: 'Jennifer Davis', birthday: '2012-12-12', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/B0E0E6/4682B4.png', maritalStatus: 'Single' },
+      { email: 'patricia.davis@example.com', status: 'Active', relation: 'Spouse', name: 'Patricia Davis', birthday: '1982-08-20', phone: '555-0116', memberPhotoUrl: 'https://placehold.co/128x128/B0E0E6/4682B4.png', subGroups: ['Womens League'], maritalStatus: 'Married', weddingDay: '2008-08-20' },
+      { email: 'robert.davis@example.com', status: 'Active', relation: 'Son', name: 'Robert Davis', birthday: '2010-10-10', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/B0E0E6/4682B4.png', subGroups: ['Altar'], maritalStatus: 'Single' },
+      { email: 'jennifer.davis@example.com', status: 'Active', relation: 'Daughter', name: 'Jennifer Davis', birthday: '2012-12-12', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/B0E0E6/4682B4.png', subGroups: ['Sunday School'], maritalStatus: 'Single' },
     ],
     familyName: 'Davis Family',
     familyId: '22/PM/0007',
-    subGroups: ['Choir', 'Men\'s Fellowship'],
+    subGroups: ['Choir'],
     memberPhotoUrl: 'https://placehold.co/128x128/AED6F1/1B4F72.png',
     zone: 'West Zone',
     ward: 'Ward 1',
@@ -275,10 +276,11 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2000-05-15',
     family: [
-      { email: 'richard.miller@example.com', status: 'Active', relation: 'Spouse', name: 'Richard Miller', birthday: '1974-01-01', phone: '555-0117', memberPhotoUrl: 'https://placehold.co/128x128/F08080/800000.png', maritalStatus: 'Married', weddingDay: '2000-05-15' }
+      { email: 'richard.miller@example.com', status: 'Active', relation: 'Spouse', name: 'Richard Miller', birthday: '1974-01-01', phone: '555-0117', memberPhotoUrl: 'https://placehold.co/128x128/F08080/800000.png', subGroups: ['Elders Forum'], maritalStatus: 'Married', weddingDay: '2000-05-15' }
     ],
     familyName: 'Miller Family',
     familyId: '15/AM/0008',
+    subGroups: ['Womens League'],
     memberPhotoUrl: 'https://placehold.co/128x128/F5CBA7/7E5109.png',
     zone: 'West Zone',
     ward: 'Ward 2',
@@ -300,7 +302,7 @@ export const members: Member[] = [
     family: [],
     familyName: 'Wilson Family',
     familyId: '23/PM/0009',
-    subGroups: ['Youth League'],
+    subGroups: ['Youth Association'],
     memberPhotoUrl: 'https://placehold.co/128x128/E6B0AA/641E16.png',
     zone: 'North Zone',
     ward: 'Ward 3',
@@ -321,13 +323,13 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2012-01-22',
     family: [
-        { email: 'william.moore@example.com', status: 'Active', relation: 'Spouse', name: 'William Moore', birthday: '1987-11-11', phone: '555-0118', memberPhotoUrl: 'https://placehold.co/128x128/D8BFD8/4B0082.png', maritalStatus: 'Married', weddingDay: '2012-01-22' },
-        { email: 'jessica.moore@example.com', status: 'Active', relation: 'Daughter', name: 'Jessica Moore', birthday: '2014-02-02', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/D8BFD8/4B0082.png', maritalStatus: 'Single' },
-        { email: 'daniel.moore@example.com', status: 'Active', relation: 'Son', name: 'Daniel Moore', birthday: '2016-03-03', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/D8BFD8/4B0082.png', maritalStatus: 'Single' }
+        { email: 'william.moore@example.com', status: 'Active', relation: 'Spouse', name: 'William Moore', birthday: '1987-11-11', phone: '555-0118', memberPhotoUrl: 'https://placehold.co/128x128/D8BFD8/4B0082.png', subGroups: ['Elders Forum'], maritalStatus: 'Married', weddingDay: '2012-01-22' },
+        { email: 'jessica.moore@example.com', status: 'Active', relation: 'Daughter', name: 'Jessica Moore', birthday: '2014-02-02', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/D8BFD8/4B0082.png', subGroups: ['Sunday School'], maritalStatus: 'Single' },
+        { email: 'daniel.moore@example.com', status: 'Active', relation: 'Son', name: 'Daniel Moore', birthday: '2016-03-03', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/D8BFD8/4B0082.png', subGroups: ['Altar'], maritalStatus: 'Single' }
     ],
     familyName: 'Moore Family',
     familyId: '18/PM/0010',
-    subGroups: ['Sunday School Teacher', 'Charity Wing'],
+    subGroups: ['Sunday School'],
     memberPhotoUrl: 'https://placehold.co/128x128/A9DFBF/145A32.png',
     zone: 'South Zone',
     ward: 'Ward 3',
@@ -349,6 +351,7 @@ export const members: Member[] = [
     family: [],
     familyName: 'Taylor Family',
     familyId: '19/AM/0011',
+    subGroups: ['Youth Association'],
     memberPhotoUrl: 'https://placehold.co/128x128/F4D03F/7D6608.png',
     zone: 'East Zone',
     ward: 'Ward 3',
@@ -369,13 +372,13 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2016-11-06',
     family: [
-        { email: 'lisha.shaji@example.com', status: 'Active', relation: 'Spouse', name: 'Lisha Elizabeth Shaji', birthday: '1990-05-25', phone: '9449835249', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Choir'], maritalStatus: 'Married', weddingDay: '2016-11-06' },
+        { email: 'lisha.shaji@example.com', status: 'Active', relation: 'Spouse', name: 'Lisha Elizabeth Shaji', birthday: '1990-05-25', phone: '9449835249', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Womens League'], maritalStatus: 'Married', weddingDay: '2016-11-06' },
         { email: 'bryanna.cheruvathur@example.com', status: 'Active', relation: 'Daughter', name: 'Bryanna Mary Cheruvathur', birthday: '2019-03-28', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Sunday School'], maritalStatus: 'Single' },
-        { email: 'meena.ippurukutty@example.com', status: 'Active', relation: 'Mother', name: 'Meena Ippurukutty', birthday: '1990-05-25', phone: '9449835249', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Choir'], maritalStatus: 'Widowed', weddingDay: '1979-01-15' }
+        { email: 'meena.ippurukutty@example.com', status: 'Active', relation: 'Mother', name: 'Meena Ippurukutty', birthday: '1990-05-25', phone: '9449835249', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Elders Forum'], maritalStatus: 'Widowed', weddingDay: '1979-01-15' }
     ],
     familyName: 'Cheruvathur Family',
     familyId: '23/PM/0012',
-    subGroups: ['Youth League', 'Choir'],
+    subGroups: ['Choir'],
     memberPhotoUrl: 'https://placehold.co/128x128/85C1E9/21618C.png',
     zone: 'West Zone',
     ward: 'Ward 3',
@@ -396,12 +399,12 @@ export const members: Member[] = [
     maritalStatus: 'Married',
     weddingDay: '2018-02-18',
     family: [
-        { email: 'thomas.anderson@example.com', status: 'Active', relation: 'Spouse', name: 'Thomas Anderson', birthday: '1990-01-01', phone: '555-0119', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Men\'s Fellowship'], maritalStatus: 'Married', weddingDay: '2018-02-18' },
-        { email: 'sarah.anderson@example.com', status: 'Active', relation: 'Daughter', name: 'Sarah Anderson', birthday: '2020-10-10', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', maritalStatus: 'Single' }
+        { email: 'thomas.anderson@example.com', status: 'Active', relation: 'Spouse', name: 'Thomas Anderson', birthday: '1990-01-01', phone: '555-0119', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Society'], maritalStatus: 'Married', weddingDay: '2018-02-18' },
+        { email: 'sarah.anderson@example.com', status: 'Active', relation: 'Daughter', name: 'Sarah Anderson', birthday: '2020-10-10', phone: 'N/A', memberPhotoUrl: 'https://placehold.co/128x128/FFC0CB/800000.png', subGroups: ['Sunday School'], maritalStatus: 'Single' }
     ],
     familyName: 'Anderson Family',
     familyId: '20/AM/0013',
-    subGroups: ['Youth League', 'Choir'],
+    subGroups: ['Womens League'],
     memberPhotoUrl: 'https://placehold.co/128x128/85C1E9/21618C.png',
     zone: 'West Zone',
     ward: 'Ward 3',
